c79f0f111baba38d6e595456a8cbcf4064fb62e1
Dano <bookemdano@gmail.com>
2025-02-01 09:52:51 -0500

    Initial 5

---
+++ danreps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  danreps
+//
+//  Created by Daniel Francis on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, monde!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ danreps/danrepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  danrepsApp.swift
+//  danreps
+//
+//  Created by Daniel Francis on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct danrepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
